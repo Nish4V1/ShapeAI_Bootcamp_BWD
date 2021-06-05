6e457c8f556830a2941a82fa4c09c148d1e62e89
Nish4V1 <85403836+Nish4V1@users.noreply.github.com>
2021-06-05 20:26:19 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        It is a 7 day bootcamp on Web development with Javascript and React.js .
+        We have learnt Javascript and React.js in this bootcamp.
+      </p>
+      <p>
+        This was a free bootcamp with project in return we were even certified.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ Note1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note1() {
+  return (
+    <div className="note">
+      <h1>Python and Machine Learning</h1>
+      <p>
+        It is also a 7 day bootcamp on Python and Machine Learning which was
+        conducted simultaneously.
+      </p>
+    </div>
+  );
+}
+
+export default Note1;
